451b87b16fd45b182a22ee2283b5511feaef8457
ysult <ysult@LAPTOP-CD24GOPF>
2019-04-08 13:37:19 -0500

    commiting bank test

--- TechfiosTestNg/src/test/java/Banking/BankTest.java
+++ TechfiosTestNg/src/test/java/Banking/BankTest.java
@@ -1,5 +1,34 @@
 package Banking;
 
+import org.junit.Test;
+
 public class BankTest {
 
+	@Test
+	public void AddAccount() {
+		
+		System.out.println("this is a testcase");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
